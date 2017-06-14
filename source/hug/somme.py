a35db90d87a0c07771b35e267c28ccdeba2403e1
OmbicheFlag <charles.ombangndo@he-arc.ch>
2017-06-14 18:37:57 +0200

    Create somme.py

---
+++ source/hug/somme.py
@@ -0,0 +1,7 @@
+"""Simple application effectuant une somme de deux nombres"""
+import hug
+
+@hug.get()
+def somme(val1, val2):
+  """Retourne la somme des deux nombres passés en paramètre"""
+  return val1 + val2
