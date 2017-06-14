9b2899ba12d6b847a8820188869e75f529c30b7c
OmbicheFlag <charles.ombangndo@he-arc.ch>
2017-06-14 22:01:55 +0200

    Delete somme.py

--- source/hug/somme.py
+++
@@ -1,7 +0,0 @@
-"""Simple application effectuant une somme de deux nombres"""
-import hug
-
-@hug.get()
-def somme(val1, val2):
-  	"""Retourne la somme des deux nombres passés en paramètre"""
-  	return val1 + val2
